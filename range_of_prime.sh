7bdc30c312919951a4b8fd67a7334450d80aa399
nayab jahan <nayabzafir@gmail.com>
2022-06-08 18:11:14 +0530

    Add:day6

---
+++ range_of_prime.sh
@@ -0,0 +1,19 @@
+echo "range of prime numbers"
+read -p "enter the two number where to find prime num:"m,n
+read -p "numbers are :$m $n"
+for a in $(seq $m $n)
+do
+    k=0
+    for i in $(seq 2 $(expr $a - 1))
+    do
+        if [ $(expr $a % $i) -eq 0 ]
+        then
+            k=1
+            break
+        fi
+    done
+    if [ $k -eq 0 ]
+    then
+    echo $a
+    fi
+done
